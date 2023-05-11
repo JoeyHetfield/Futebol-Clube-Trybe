438fc5ffe152df7d75ae2ab9c720c1be9bc070e9
Joey Hetfield <joeydefenix@hotmail.com>
2023-05-11 18:28:11 -0300

    validação do bcrypt

--- app/backend/src/service/LoginService.ts
+++ app/backend/src/service/LoginService.ts
@@ -1,3 +1,4 @@
+import * as bcrypt from 'bcryptjs';
 import UserModel from '../model/UserModel';
 import ErrorFile from '../utils/ErrorFile';
 import Jwt from '../utils/auth';
@@ -9,6 +10,12 @@ class LoginService {
     const user = await this.userModel.getUserByMail(email);
     if (!user || !password) {
       throw new ErrorFile('All fields must be filled', 400);
+    }
+
+    const isPassword = await bcrypt.compare(password, user.password);
+
+    if (!isPassword) {
+      throw new ErrorFile('Invalid password', 400);
     }
     const token = this.jwt.createToken({ id: user.id, email });
     return token;
