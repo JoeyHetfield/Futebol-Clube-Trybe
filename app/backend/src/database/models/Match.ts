e0b4b72b3b11fe73e7489e969219df2e25edeb45
Joey Hetfield <joeydefenix@hotmail.com>
2023-05-11 17:10:28 -0300

    feito model e migration de Match, descomentando a seeder

---
+++ app/backend/src/database/models/Match.ts
@@ -0,0 +1,47 @@
+import { Model, DataTypes } from 'sequelize';
+import db from '.';
+import Team from './Team';
+
+class Match extends Model {
+  declare homeTeamId: number;
+  declare awayTeamId: number;
+  declare homeTeamGoals: number;
+  declare awayTeamGoals: number;
+  declare inProgress: boolean;
+}
+
+Match.init({
+  homeTeamId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  awayTeamId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  homeTeamGoals: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  awayTeamGoals: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  inProgress: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false,
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  tableName: 'matches',
+  timestamps: false,
+});
+
+Match.belongsTo(Team, { foreignKey: 'homeTeamId', as: 'homeTeam' });
+Match.belongsTo(Team, { foreignKey: 'awayTeamId', as: 'awayTeam' });
+
+Team.hasMany(Match, { foreignKey: 'homeTeamId', as: 'homeTeam' });
+Team.hasMany(Match, { foreignKey: 'awayTeamId', as: 'awayTeam' });
+
+export default Match;
