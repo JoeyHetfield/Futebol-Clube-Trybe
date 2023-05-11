ce9fbf436ffe47e35d8978945417c2cdd95887a0
Joey Hetfield <joeydefenix@hotmail.com>
2023-05-11 14:30:18 -0300

    tipei os parametros do handleError, fiz a mesma base usada no blogs-api

--- app/backend/src/middlewares/handleError.ts
+++ app/backend/src/middlewares/handleError.ts
@@ -1,6 +1,7 @@
+import { Request, Response, NextFunction } from 'express';
 import ErrorFile from '../utils/ErrorFile';
 
-export default (err, _req, res, _next) => {
+export default (err: Error, _req: Request, res: Response, _next: NextFunction) => {
   if (err instanceof ErrorFile) {
     return res.status(err.status).json({ message: err.message });
   }
