e56917e8c017ba8b937a6ba02bcae9552de84e4c
Joey Hetfield <joeydefenix@hotmail.com>
2023-05-12 19:18:15 -0300

    criando primeiro teste para match, criando também um mock com a lista de todas as partidas

---
+++ app/backend/src/tests/match.test.ts
@@ -0,0 +1,35 @@
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+
+// @ts-ignore
+import chaiHttp = require('chai-http');
+
+import { app } from '../app';
+
+import MatchModel from '../model/MatchModel';
+import getMatchesMock from './mock/match.mock';
+import Match from '../database/models/Match';
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+describe('Se getMatches funciona', () => {
+  let matchModelStub: sinon.SinonStub;
+
+  beforeEach(() => {
+    matchModelStub = sinon.stub(Match, 'findAll').resolves(getMatchesMock as unknown as Match[]);
+  })
+
+  afterEach(() => {
+    matchModelStub.restore();
+  })
+
+  it('getMatches retorna todos os jogos', async () => {
+    const { status, body } = await chai.request(app).get('/matches');
+
+    expect(status).to.be.equal(200);
+    expect(body).to.be.deep.equal(getMatchesMock);
+  
+  })
+})
