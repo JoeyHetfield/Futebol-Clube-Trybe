9e65ab242ea6155c8400edd450a7dbf5d74581ec
Joey Hetfield <joeydefenix@hotmail.com>
2023-05-12 12:57:05 -0300

    criando estrutura para pegar o role, só não está funcionando ainda

--- app/backend/src/Routes/LoginRouter.ts
+++ app/backend/src/Routes/LoginRouter.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import LoginController from '../controller/LoginController';
 import validateLogin from '../middlewares/validateLogin';
+import validateToken from '../middlewares/validateToken';
 
 const loginRouter = Router();
 const loginController = new LoginController();
 
 loginRouter.post('/', validateLogin, (_req, res) => loginController.login(_req, res));
+loginRouter.get('/role', validateToken, (_req, res) => LoginController.loginRole(_req, res));
 
 export default loginRouter;
--- app/backend/src/controller/LoginController.ts
+++ app/backend/src/controller/LoginController.ts
@@ -9,6 +9,11 @@ class LoginController {
     const token = await this.loginService.login(email, password);
     res.status(200).json({ token });
   }
+
+  static async loginRole(req: Request, res: Response) {
+    const { role } = req.body;
+    res.status(200).json({ role });
+  }
 }
 
 export default LoginController;
