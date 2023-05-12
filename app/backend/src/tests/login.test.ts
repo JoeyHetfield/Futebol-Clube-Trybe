6d8a2f28a4c7d29a604d25754737860b27f845c7
Joey Hetfield <joeydefenix@hotmail.com>
2023-05-12 12:08:53 -0300

    aparentemente testes de login feito

--- app/backend/src/tests/login.test.ts
+++ app/backend/src/tests/login.test.ts
@@ -14,16 +14,34 @@ describe('Testando o Login', () => {
   afterEach(sinon.restore);
 
   it('Retorna erro quando não passa o email', async () => {
-    const { status, body } = await chai.request(app).post('/login').send({user: '', password: 'secret_admin'});
+    const { status, body } = await chai.request(app).post('/login').send({email: '', password: 'secret_admin'});
 
     expect(status).to.be.equal(400);
     expect(body.message).to.be.equal('All fields must be filled');
   })
   it('Retorna erro quando não passa a senha', async () => {
-    const { status, body } = await chai.request(app).post('/login').send({user: 'admin@admin.com', password: ''});
+    const { status, body } = await chai.request(app).post('/login').send({email: 'admin@admin.com', password: ''});
 
     expect(status).to.be.equal(400);
     expect(body.message).to.be.equal('All fields must be filled');
+  })
+  it('Retorna erro se passa um email inválido', async () => {
+    const { status, body } = await chai.request(app).post('/login').send({email: 'ASHCIUHSHadmcom', password: 'secret_admin'});
+
+    expect(status).to.be.equal(401);
+    expect(body.message).to.be.equal('Invalid email or password');
+  })
+  it('Retorna erro se passa uma senha inválida', async () => {
+    const { status, body } = await chai.request(app).post('/login').send({email: 'admin@admin', password:'123'});
+
+    expect(status).to.be.equal(401);
+    expect(body.message).to.be.equal('Invalid email or password');
+  })
+  it('Funciona corretamente se passa os dados válidos', async () => {
+    const { status, body } = await chai.request(app).post('/login').send({email: 'admin@admin.com', password: 'secret_admin'});
+
+    expect(status).to.be.equal(200);
+    expect(body.token).to.be.a('string');
   }
   );
 });
