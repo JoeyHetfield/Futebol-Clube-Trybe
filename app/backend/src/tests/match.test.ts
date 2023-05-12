3d644bcaccc4280d501341ac4e3bb6a575fe18ba
Joey Hetfield <joeydefenix@hotmail.com>
2023-05-12 19:54:23 -0300

    feito os testes do geMatches, agora falta passar o token para os testes

--- app/backend/src/tests/match.test.ts
+++ app/backend/src/tests/match.test.ts
@@ -7,7 +7,8 @@ import chaiHttp = require('chai-http');
 import { app } from '../app';
 
 import getMatchesMock from './mock/match.mock';
-import getMatchesInProgressMock from './mock/match.mock';
+import getMatchesInProgessTrueMock from './mock/match.mock';
+import getMatchesInProgessFalseMock from './mock/match.mock';
 import Match from '../database/models/Match';
 
 chai.use(chaiHttp);
@@ -31,4 +32,16 @@ describe('Se getMatches funciona', () => {
     expect(status).to.be.equal(200);
     expect(body).to.be.deep.equal(getMatchesMock);
   })
+  it('getMatches retorna todos os jogos em andamento', async () => {
+    const { status, body } = await chai.request(app).get('/matches?inProgress=true');
+
+    expect(status).to.be.equal(200);
+    expect(body).to.be.deep.equal(getMatchesInProgessTrueMock);
+  });
+  it('getMatches retorna todos os jogos finalizados', async () => {
+    const { status, body } = await chai.request(app).get('/matches?inProgress=false');
+
+    expect(status).to.be.equal(200);
+    expect(body).to.be.deep.equal(getMatchesInProgessFalseMock);
+  });
 });
